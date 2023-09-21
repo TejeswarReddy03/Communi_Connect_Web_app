0d67653d65152cfacbdc570f3a0d37b54fc94af4
tejeswar <tejeswarreddysunkugari03@gmail.com>
2023-09-22 00:41:06 +0530

    fetching data from frontend and keeping it in mongodb is done

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require("cors");
 const db = require('./config/mongoose');
-const port = 8000;
+const port = 8004;
 const app=express();
 app.use(express.json());
 const User = require("./models/user");
@@ -10,19 +10,93 @@ app.get("/gett",function(req,res){
     res.send("running!!!");
     //console.log(req);
 });
+/*
+const create=function(req,res){
+    if(req.body.password!=req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email:req.body.email})
+.then((user)=>{
+        if(!user){
+            User.create(
+                req.body
+            )
+          .then((user) => {
+            console.log("created");
+            return res.redirect('/users/signin');
+          })
+          .catch((err) => {
+            console.log("error in signing up",err);
+            return;
+          });
+        }
+        else{
+            return res.redirect('back');
+        }
+    })
+    .catch((err) => {
+            console.log("error in finding user in signing up");
+            return;
+          });
+};
+
 
 
 app.post('/gets', async (req, res) => {
     try {
+        
         console.log(req.body); // Log the request body
         res.status(200).json({ message: 'Data received successfully' }); // Respond to the client
     } catch (error) {
         console.error('Error saving data:', error);
         res.status(500).json({ message: 'Internal server error' });
     }
-});
+},create);
+
+*/
+const create = async (req, res) => {
+    console.log("HH");
+    try {
+      if (req.body.password !== req.body.confirm_password) {
+        return res.status(400).json({ message: "Password and confirm_password do not match" });
+      }
+  
+      const existingUser = await User.findOne({ email: req.body.email });
+  
+      if (existingUser) {
+        return res.status(400).json({ message: "User with this email already exists" });
+      }
+  
+      const newUser = new User({
+        name: req.body.user_name,
+        email: req.body.email,
+        pincode: req.body.pincode,
+        password: req.body.password,
+      });
+      console.log("User created");
+
+      await newUser.save(); // Save the new user to the database
+  
+      console.log("User created");
+      return res.status(201).json({ message: "User created successfully" });
+    } catch (error) {
+      console.error("Error in user creation:", error);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  };
 
 
+
+  app.post('/gets', async (req, res) => {
+    try {
+      console.log(req.body); // Log the request body
+      await create(req, res); // Call the create function to create the user
+    //  res.status(200).json({ message: 'Data received and user created successfully' }); // Respond to the client
+    } catch (error) {
+      console.error('Error saving data:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
 
 
 app.listen(port,function (err){
--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -1,21 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios"; 
+
 
 function Signup(){
+    const [formData, setFormData] = useState({
+        user_name: "",
+        email: "",
+        pincode: "",
+        password: "",
+        confirm_password: "",
+      });
+      const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+          ...formData,
+          [name]: value,
+        });
+      };
+
+
+
+
+    
+      const handleSubmit = async (event) => {
+        event.preventDefault(); // Prevent the default form submission behavior
+    
+        try {
+          // Make an HTTP POST request to your backend server
+          const response = await axios.post("http://localhost:8004/gets", formData); // Replace "/api/register" with your backend endpoint
+          console.log("Registration successful", response.data);
+    
+          // Optionally, you can redirect the user or perform other actions
+        } catch (error) {
+          console.error("Registration failed", error);
+        }
+      };
+    
+
     return(
         <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
             <div className="bg=white p-3 rounded w-25">
                 <h2>Register</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                 <div className="mb-3">
-                    <label htmlFor="email" >
-                        <strong>Name</strong>
+                    <label htmlFor="name" >
+                        <strong>name</strong>
                     </label>
                     <input
                       type="text"
                       placeholder="Enter Name"
                       autoComplete="off"
-                      name="Name"
+                      name="user_name"
                       className="form-control rounded-10"
+                      
+                      onChange={handleInputChange}
                     />
                     </div>
                     <div className="mb-3">
@@ -28,6 +66,8 @@ function Signup(){
                             autoComplete="off"
                             name="email"
                             className="form-control rounded-10"
+                            
+                            onChange={handleInputChange}
                         />
                         </div>  
                         <div className="mb-3">
@@ -40,6 +80,8 @@ function Signup(){
                                 autoComplete="off"
                                 name="pincode"
                                 className="form-control rounded-10"
+                               
+                                onChange={handleInputChange}
                             />
                         </div>
                         <div className="mb-3">
@@ -51,6 +93,8 @@ function Signup(){
                                 placeholder="Enter Password"
                                 name = "password"
                                 className="form-control rounded-10"
+                                
+                                onChange={handleInputChange}
                             />
                         </div>
                         <div className="mb-3">
@@ -62,16 +106,19 @@ function Signup(){
                                 placeholder="Confirm Password"
                                 name = "confirm_password"
                                 className="form-control rounded-10"
+                                
+                                onChange={handleInputChange}
                             />
                         </div>
                         <button type="submit" className="btn btn-success w-100 rounded-10">
                             Register
                         </button>
-                        <p><center>Already Have an Account??</center></p>
+                        
+                </form>
+                <p><center>Already Have an Account??</center></p>
                         <button className = "btn btn-default border w-100 bg-light rounded-10 text-decoration-none">
                             Login
                         </button>
-                </form>
             </div>
         </div>
 
