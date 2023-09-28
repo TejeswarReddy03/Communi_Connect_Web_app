7f9a181d1bef56f9fb652849d35ffa83c3d66750
Shanmukhsai22 <shanmukhsai22.45@gmail.com>
2023-09-28 22:27:09 +0530

    fixed maps.js

--- frontend/src/components/maps.js
+++ frontend/src/components/maps.js
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from 'react';
 import Markonmap from './markonmap';
 function Maps(){
 
-    // return (
-    //     <iframe
-    //         title="HTML Content"
-    //         src="../../maps.html" // Adjust the path as needed
+    return (
+        <iframe
+            title="HTML Content"
+            src="../../maps.html" // Adjust the path as needed
             
-    //         width="100%"
-    //         height="900px"
-    //     />
-    // );
+            width="100%"
+            height="900px"
+        />
+    );
 }
 export default Maps;
