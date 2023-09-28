3b5b281d067c8ca89445ecbce22931090c4a4786
Shanmukhsai22 <shanmukhsai22.45@gmail.com>
2023-09-28 22:23:19 +0530

    modified maps.js

---
+++ frontend/src/components/maps.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import Markonmap from './markonmap';
+function Maps(){
+
+    return (
+        <iframe
+            title="HTML Content"
+            src="../../maps.html" // Adjust the path as needed
+            
+            width="100%"
+            height="900px"
+        />
+    );
+}
+export default Maps;
