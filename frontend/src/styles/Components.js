31461df1a4d025bd2d62216d6eaa21bb936a09da
manoj <manojkumargompa@gmail.com>
2023-10-24 14:04:05 +0530

    modified frontpage fontcolor

--- frontend/src/styles/Components.js
+++ frontend/src/styles/Components.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   background-color: #fff;
+  color:black;
   border-radius: 10px;
   box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
   position: relative;
