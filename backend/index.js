4442652fa5962ba0d13da1c00b31789decc235e2
Manojkumargmpa <110841909+Manojkumargmpa@users.noreply.github.com>
2023-11-20 00:16:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ cloudinary.config({
 const nodemailer = require('nodemailer');
 const io = require('socket.io')(8080, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://communiconnectwebapp.onrender.com',
   }
 });
 const app = express();
